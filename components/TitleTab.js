0084c8e47ef03246f45bafecb5ca0706649d84ae
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-11 19:13:01 +0530

    Add data table

--- components/TitleTab.js
+++ components/TitleTab.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useCallback } from 'react';
 import {Card, Tabs} from '@shopify/polaris';
 import Link from 'next/link';
 const TitleTab = () => {
