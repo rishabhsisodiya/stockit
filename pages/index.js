784714eb5ab412ee75ff6fc356e58af3f313f410
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-11 15:28:00 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+class Index extends React.Component {
+  state = { open: false };
+
+  render() {
+    return (
+      <div>
+        <p>Sample app using React and Next.js</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
