6364949f66930b04f109ac262bf56e0f9cfdadd0
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-13 01:16:08 +0530

    Add event handler for textbox

--- components/TestList.js
+++ components/TestList.js
@@ -42,8 +42,8 @@ function TestProductList() {
   //CheckBox selectable
   // const [selectedItems, setSelectedItems] = useState([]);
   // Handle Quantityy field
-  const [value, setValue] = useState('1');
-  const handleChange = useCallback((newValue) => setValue(newValue), []);
+  // const [value, setValue] = useState('1');
+  // const handleChange = useCallback((newValue) => setValue(newValue), []);
 
 
   return (
@@ -94,9 +94,9 @@ function TestProductList() {
             <Stack.Item>
               <TextField
                 type="number"
-                // value="5"
-                value={value}
-                onChange={handleChange}
+                value="5"
+                // value={value}
+                // onChange={handleChange}
               />
             </Stack.Item>
             <Stack.Item>
