b87ba7e8e3e961b59293af7e6a04a7336edbcb1f
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-14 00:57:02 +0530

    test sandbox file

--- components/TitleTab.js
+++ components/TitleTab.js
@@ -5,36 +5,6 @@ import TestProductList from './TestList';
 import Sandbox from './sandbox';
 const TitleTab = () => {
 
-  const rows = [
-    [
-      <Link url="https://www.example.com" key="emerald-silk-gown">
-        Emerald Silk Gown
-      </Link>,
-      '$875.00',
-      124689,
-      140,
-      '$122,500.00',
-    ],
-    [
-      <Link url="/products" key="mauve-cashmere-scarf">
-        Mauve Cashmere Scarf
-      </Link>,
-      '$230.00',
-      124533,
-      83,
-      '$19,090.00',
-    ],
-    [
-      <Link url="/products" key="navy-merino-wool">
-        Navy Merino Wool Blazer with khaki chinos and yellow belt
-      </Link>,
-      '$445.00',
-      124518,
-      32,
-      '$14,240.00',
-    ],
-  ];
-
   const [selected, setSelected] = useState(0);
    
     const handleTabChange = useCallback(
@@ -64,20 +34,8 @@ const TitleTab = () => {
 return (
   <Card>
     <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
-        {/* <Card.Section title={tabs[selected].content}> */}
-        {/* <DataTable
-          columnContentTypes={[
-            'text',
-            'numeric',
-            'numeric',
-            'numeric',
-            'numeric',
-          ]}
-          headings={['Product', 'Price', 'SKU Number', 'Quantity', 'Net sales']}
-          rows={rows}
-        /> */}
-        <TestProductList/>
-        {/* <Sandbox/> */}
+        {/* <TestProductList/> */}
+        <Sandbox/>
         {/* </Card.Section> */}
     </Tabs>
     {/* <ProductList/> */}
