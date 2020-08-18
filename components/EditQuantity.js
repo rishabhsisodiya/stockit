425666ba1963fac64ec7251255e5f039d0ae3a16
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-19 02:22:20 +0530

    update query inventorylevel

--- components/EditQuantity.js
+++ components/EditQuantity.js
@@ -32,8 +32,8 @@ query getInventoryItemByID($id: ID!) {
 `;
 
 const EditQuantity = (props) => {
-    const { loading, error, inventoryItemdata } = useQuery(GET_INVENTORY_ITEM_BY_ID,{ variables: { id: props.variantId } });
-    console.log(inventoryItemdata)
+    const { loading, error, data } = useQuery(GET_INVENTORY_ITEM_BY_ID,{ variables: { id: props.variantId } });
+    console.log(props.variantId+":"+data)
     // console.log(inventoryItemdata.inventoryItem.id)
     // const { loading, error, inventoryLevelsdata } = useQuery(GET_INVENTORY_LEVELS_BY_ID,{ variables: { id: inventoryItemdata.inventoryItem.id } });
     // console.log('inventoryleveldata:',inventoryLevelsdata);
