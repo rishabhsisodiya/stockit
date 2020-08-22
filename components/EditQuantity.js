5b696423add24ad05e7e6124f5c5cf5bb851a231
Rishabh <rishabh.sisodiya4@gmail.com>
2020-08-23 02:37:58 +0530

    testing storing data functionality

--- components/EditQuantity.js
+++ components/EditQuantity.js
@@ -42,20 +42,16 @@ const EditQuantity = (props) => {
         },
         [],
     )
-    const updateHandler = useCallback(
-      () => {
-        addQuantity({ variables: { 
-          inventoryAdjustQuantityInput: {
-            inventoryLevelId:data.inventoryItem.inventoryLevels.edges[0].node.id,
-            availableDelta:parseInt(value)
-          } 
-        } })
-        setvalue('');
-        props.callback();
-      },
-      [],
-    );
-      
+    const updateHandler = () => {
+      addQuantity({ variables: { 
+      inventoryAdjustQuantityInput: {
+        inventoryLevelId:data.inventoryItem.inventoryLevels.edges[0].node.id,
+        availableDelta:parseInt(value)
+      } 
+    } })
+    setvalue('');
+    props.callback();
+  };
   // const { loading, error, data } = useQuery(GET_INVENTORY_ITEM_BY_ID,{ variables: { id: props.variantId } });
   const { loading, error, data } = useQuery(GET_INVENTORY_LEVELS_BY_ID,{ variables: { id: props.inventoryId } });
   const [ addQuantity, {mloading,merror,mdata} ] = useMutation(UPDATE_QUANTITY);
