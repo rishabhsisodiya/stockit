1a856dd1b6c83be43c2ab3024b3ece3f267d1f46
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-04 16:37:05 +0530

    shift back

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,16 @@
-import { Page, Layout, Frame } from "@shopify/polaris";
-import React, { useState, useCallback } from "react";
-import { Card, Tabs } from "@shopify/polaris";
-import ProductList from "../components/ProductList";
-import TestProductList from "../components/TestList";
-import Sandbox from "../components/sandbox";
+import {Page, Layout, Frame} from '@shopify/polaris';
+import TitleTab from '../components/TitleTab';
 
 class Index extends React.Component {
-  // Frame for displaying toast
+// Frame for displaying toast
   render() {
-    console.log("index rendering");
-    const [selected, setSelected] = useState(0);
-
-    const handleTabChange = useCallback(
-      (selectedTabIndex) => setSelected(selectedTabIndex),
-      [selected]
-    );
-
-    const tabs = [
-      {
-        id: "Dev",
-        content: "Dev",
-        accessibilityLabel: "Dev",
-        panelID: "Dev",
-      },
-      {
-        id: "Pre-Prod",
-        content: "Pre-Prod",
-        accessibilityLabel: "Pre-Prod",
-        panelID: "Pre-Prod",
-      },
-      {
-        id: "Prod",
-        content: "Prod",
-        accessibilityLabel: "Prod",
-        panelID: "Prod",
-      },
-    ];
-
-    let tabSelected;
-    if (selected == 0) {
-      tabSelected = <Sandbox />;
-    }
-    if (selected == 1) {
-      tabSelected = <TestProductList />;
-    }
-    if (selected == 2) {
-      tabSelected = <ProductList />;
-    }
+    console.log('index rendering');
     return (
       <Frame>
         <Page>
           <Layout>
             <Layout.Section>
-              <Card>
-                <Tabs
-                  tabs={tabs}
-                  selected={selected}
-                  onSelect={handleTabChange}
-                >
-                  {tabSelected}
-                </Tabs>
-              </Card>
+              <TitleTab/>
             </Layout.Section>
           </Layout>
         </Page>
