5553662c83b8472398eb9645be7a1cb2013c6eeb
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-05 23:56:48 +0530

    working on custom search

--- components/TitleTab.js
+++ components/TitleTab.js
@@ -8,43 +8,49 @@ import Products from './Products';
 const TitleTab = () => {
 
   const [selected, setSelected] = useState(0);
+  const [tabs, setTabs] = useState({
+    id: 'Dev',
+    content: 'Dev',  
+    accessibilityLabel: 'Dev',  
+    panelID: 'Dev',  
+    });
    
     const handleTabChange = useCallback(
       (selectedTabIndex) => setSelected(selectedTabIndex),
       [selected],
     );
       
-    let tabs = [
-        {
-        id: 'Dev',
-        content: 'Dev',  
-        accessibilityLabel: 'Dev',  
-        panelID: 'Dev',  
-        },  
-        {  
-        id: 'Pre-Prod',  
-        content: 'Pre-Prod',
-        accessibilityLabel: 'Pre-Prod',  
-        panelID: 'Pre-Prod',  
-        }, 
-        {  
-        id: 'Prod',  
-        content: 'Prod', 
-        accessibilityLabel: 'Prod', 
-        panelID: 'Prod',  
-        },   
-    ];
+    // let tabs = [
+    //     {
+    //     id: 'Dev',
+    //     content: 'Dev',  
+    //     accessibilityLabel: 'Dev',  
+    //     panelID: 'Dev',  
+    //     },  
+    //     {  
+    //     id: 'Pre-Prod',  
+    //     content: 'Pre-Prod',
+    //     accessibilityLabel: 'Pre-Prod',  
+    //     panelID: 'Pre-Prod',  
+    //     }, 
+    //     {  
+    //     id: 'Prod',  
+    //     content: 'Prod', 
+    //     accessibilityLabel: 'Prod', 
+    //     panelID: 'Prod',  
+    //     },   
+    // ];
 console.log(tabs);
     const customSearchHandler = useCallback(
       () => {
-        tabs.push({  
+        let customSearch= {  
           id: 'CustomSearch',  
           content: 'CustomSearch', 
           accessibilityLabel: 'CustomSearch', 
           panelID: 'CustomSearch',  
-          })
+          }
+        setTabs([...tabs,customSearch])
          let newTabIndex = tabs.length-1;
-         console.log(tabs);
          setSelected(newTabIndex); 
       },
       [tabs],
@@ -59,15 +65,16 @@ console.log(tabs);
     let tabSelected;
 if (selected==0) {
   tabSelected=<Sandbox customSearch={customSearchHandler} removeCustomSearch={removeCustomSearch}/>
-}
-if (selected==1) {
-  tabSelected=<TestProductList/>
-}
-if (selected==2) {
-  tabSelected=<ProductList/>
 }else{
   tabSelected=<Sandbox customSearch={customSearchHandler} removeCustomSearch={removeCustomSearch}/>
 }
+// if (selected==1) {
+//   tabSelected=<TestProductList/>
+// }
+// if (selected==2) {
+//   tabSelected=<ProductList/>
+// }
+
 console.log('tab rendering..');
 return (
   <Card>
