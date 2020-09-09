c785d71a7b4b3f571e798b02ac097a3840841a21
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-09 21:47:34 +0530

    update tabs using useEffect

--- components/TitleTab.js
+++ components/TitleTab.js
@@ -49,31 +49,31 @@ const TitleTab = () => {
 
   const handleTabChange = (selectedTabIndex) => setSelected(selectedTabIndex);
 
-  useEffect(() => {
-    console.log("UseEffect", data);
-    if (data) {
-      data.productSavedSearches.edges.map((item) => {
-        setTabs([
-          ...tabs,
-          {
-            id: item.node.id,
-            content: item.node.name,
-            accessibilityLabel: item.node.name,
-            panelID: item.node.id,
-          },
-        ]);
-        setFilter([
-          ...filter,
-          {
-            id: item.node.id,
-            name: item.node.name,
-            query: item.node.query,
-          },
-        ]);
-      });
-    }
-    console.log("Data:", filter, tabs);
-  }, [data, tabs, filter]);
+  // useEffect(() => {
+  //   console.log("UseEffect", data);
+  //   if (data) {
+  //     data.productSavedSearches.edges.map((item) => {
+  //       setTabs([
+  //         ...tabs,
+  //         {
+  //           id: item.node.id,
+  //           content: item.node.name,
+  //           accessibilityLabel: item.node.name,
+  //           panelID: item.node.id,
+  //         },
+  //       ]);
+  //       setFilter([
+  //         ...filter,
+  //         {
+  //           id: item.node.id,
+  //           name: item.node.name,
+  //           query: item.node.query,
+  //         },
+  //       ]);
+  //     });
+  //   }
+  //   console.log("Data:", filter, tabs);
+  // }, [data, tabs, filter]);
 
   if (loading)
     return (
